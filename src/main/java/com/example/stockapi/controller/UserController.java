ce1cbe3c8aa5c535ea3ec5a7b2ceedfdba64a484
thecoderenroute <thecoderenroute@gmail.com>
2022-02-04 11:09:37 +0530

    secured some endpoints and added cookies support

--- src/main/java/com/example/stockapi/controller/UserController.java
+++ src/main/java/com/example/stockapi/controller/UserController.java
@@ -3,17 +3,23 @@ package com.example.stockapi.controller;
 import com.example.stockapi.dao.UserDao;
 import com.example.stockapi.exception.ApiException;
 import com.example.stockapi.model.Investment;
+import com.example.stockapi.model.role.ERole;
+import com.example.stockapi.model.role.Role;
 import com.example.stockapi.model.stock.Stock;
 import com.example.stockapi.repository.StockDataRepo;
 import com.example.stockapi.model.User;
+import com.example.stockapi.security.jwt.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/api/user")
 public class UserController {
@@ -21,10 +27,13 @@ public class UserController {
     StockDataRepo stockDataRepo;
     UserDao userDao;
 
+    JwtUtils jwtUtils;
+
     @Autowired
-    public UserController(StockDataRepo stockDataRepo, UserDao userDao) {
+    public UserController(StockDataRepo stockDataRepo, UserDao userDao, JwtUtils jwtUtils) {
         this.stockDataRepo = stockDataRepo;
         this.userDao = userDao;
+        this.jwtUtils = jwtUtils;
     }
 
     private User getUserFromNumber(String userAccountNumber) throws IllegalArgumentException {
@@ -36,9 +45,6 @@ public class UserController {
         return optionalUser.get();
     }
 
-    private User getAuthenticatedUser() {
-        return null;
-    }
 
     private void updateUserInvestments(User user) {
         user.getInvestments().forEach(i -> {
@@ -50,6 +56,18 @@ public class UserController {
         });
 
         user.refresh();
+    }
+
+    @GetMapping("/checkRole")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    private ResponseEntity<Boolean> hasAuthority(HttpServletRequest httpServletRequest) {
+
+        User u = this.userDao.getById(this.jwtUtils.getUserNameFromJwtToken(this.jwtUtils.getJwtFromCookies(httpServletRequest)));
+
+        System.out.println("User: "+u);
+
+        return ResponseEntity.ok(u.getRoles().contains(new Role(ERole.ROLE_USER)));
+
     }
 
     private Investment getInvestmentFromId(User user, Integer investmentId) {
@@ -184,7 +202,7 @@ public class UserController {
     @PostMapping("/add")
     public void addUser(@RequestBody User user) {
 //        User u = new User(user.getName(), user.getEmail(), user.getMobileNumber(), user.getPassword(),user.getAccountNumber());
-        System.out.println("User rcvd: "+user);
+        System.out.println("User rcvd: " + user);
         userDao.saveAndFlush(user);
     }
 
